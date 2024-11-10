4600428aef3ae928c0d86ae4890732b190519b15
Yohannes-Asfaw <jonathanasfaw11@gmail.com>
2024-11-10 10:50:12 +0300

    some ui change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  target: 'static',
   css: ['~/assets/css/tailwind.css'],
-
+ router: {
+    base: '/Nuxt.js-frontend-selection/',
+  },
+  
   // Vite configuration
   postcss: {
     plugins: {
